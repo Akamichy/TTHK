3139ead41b695b49d46ad04d3546332e2204fbcc
yanis.pahhomov@gmail.com <yanis.pahhomov@gmail.com>
2023-11-30 14:01:58 +0200

    1

--- module3.py
+++ module3.py
@@ -14,15 +14,17 @@ else:
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("Определяем, сколько в числе чётных и сколько нечётных цифр")
     print()
-    c==b==a
-    paaris ==0
-    paaritu == 0
-    while b > 0:
-            if b % 2 == 0:
-                paaris =+ 1
-            else:
-                paaritu =+ 1
-                b = b // 10
+    b = a
+    c = a
+    paaris = 0
+    paaritu = 0
+    while not b <=  0:
+        if b % 2 == 0:
+            paaris += 1
+            b = b // 10
+        else:
+            paaritu += 1
+            b = b // 10
     
     print("Чётных цифр:", paaris)
     print("Нечётных цифр:", paaritu)
@@ -47,12 +49,9 @@ else:
         print("с - нечётное число. Умножаем на 3, прибавляем 1 и делим на 2.")
     while c != 1:
             if c % 2 == 0:
-                c == c / 2
+                c = c / 2
             else:
-                c == (3*c + 1) / 2
+                c = (3*c + 1) / 2
             print(c, end=" ")
     print()
     print("Гипотеза верна")
-
-
-    list1 = 99991111
